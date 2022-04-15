d1c736637ded80a9436daf341caacb9cd6071348
Ismail1909 <68963264+Ismail1909@users.noreply.github.com>
2022-04-15 03:36:09 +0200

    RR

--- CPU-Scheduler-Visualizer/RoundRobin.cpp
+++ CPU-Scheduler-Visualizer/RoundRobin.cpp
@@ -1,5 +1,56 @@
 #include "RoundRobin.h"
 
 GanntChart RoundRobin(const std::vector<RoundRobin_process> &processes, unsigned int timeQuantum) {
+#define NO_OF_PROCESS processes.size()
+	GanntChart ganntChart;
+	GanntChartSection section;
+	vector<RoundRobin_process> process = processes;
 
+	unsigned int last = processes.size();
+	unsigned int time = 0;
+	unsigned int i = 0;
+	bool idle = true;
+
+	while (last > 0)
+	{
+		if (process[i].arrivalTime <= time && process[i].burstLength > 0)
+		{
+			idle = false;
+			section.process = processes[i].id;
+			process[i].burstLength -= timeQuantum;
+
+			section.start = time;
+			time += timeQuantum;
+			section.end = time;
+
+			ganntChart.push_back(section);
+
+			if (process[i].burstLength <= 0)
+			{
+				last--;
+			}
+
+		}
+		else if (process[i].arrivalTime >= time)
+		{
+			idle = true;
+		}
+
+		if (i == process.size() - 1 && idle)
+		{
+			section.process = 0;
+
+			section.start = time;
+			time += timeQuantum;
+			section.end = time;
+
+			ganntChart.push_back(section);
+		}
+
+		i = (i + 1) % NO_OF_PROCESS;
+
+
+	}
+
+	return ganntChart;
 }

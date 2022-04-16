4daf73603dff05708470ee86818c48f0bf53ec58
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-04-16 20:28:36 +0200

    Update SJF.h

--- CPU-Scheduler-Visualizer/SJF.h
+++ CPU-Scheduler-Visualizer/SJF.h
@@ -11,6 +11,10 @@ typedef struct {
 } SJF_process;
 
 GanntChart SJFN(const std::vector<SJF_process> &processes);
+<<<<<<< HEAD
 GanntChart SJFP(const std::vector<SJF_process>& processes);
+=======
+GanntChart SJFP(const std::vector<SJF_process> &processes);
+>>>>>>> parent of 95eca4e (SJF)
 
 #endif // SJF_H

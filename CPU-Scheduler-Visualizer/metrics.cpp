5da70e34983af5dc756b9e6d9c4252af84962e42
BreksamAmr <85411193+BreksamAmr@users.noreply.github.com>
2022-04-17 21:45:13 +0200

    Update metrics.cpp
    function to calculate the average and maximum time

--- CPU-Scheduler-Visualizer/metrics.cpp
+++ CPU-Scheduler-Visualizer/metrics.cpp
@@ -1,5 +1,58 @@
 #include "metrics.h"
+#include <list>
+
+using namespace std;
+
+int maximum(vector<int> z)  //function to calculate the maximum element in a vector
+{
+	int max = 0;
+	for (int i = 0; i < z.size(); i++)
+	{
+		if (z[i] > max)
+		{
+			max = z[i];
+		}
+	}
+	return max;
+}
 
 metrics calculateMetrics(GanntChart timeline,  const std::vector<process> &processes) {
-
+	metrics m;
+	vector<int> turnArround;   //vector to hold the turnarround time for each process
+	double totalt = 0;
+	vector<int> waitingTime;   //vector to hold the waiting time for each process
+	double totalw = 0;
+	vector<int> responseTime;  //vector to hold the response time for each process
+	double totalr = 0;
+	list<GanntChartSection> x = timeline;
+	x.reverse();
+	for (int i = 0; i < processes.size(); i++)
+	{
+		for (GanntChartSection section : x) //iterate on reversed ganntchart to use end value of each process
+		{
+			if (processes[i].id == section.process)
+			{
+				turnArround[i] = section.end - processes[i].arrivalTime;
+				waitingTime[i] = turnArround[i] - processes[i].burstLength;
+				totalt += turnArround[i];
+				totalw += waitingTime[i];
+			}
+		}
+		for (GanntChartSection section : timeline)  //iterate on reversed ganntchart to use first time of each process gets cpu
+		{
+			if (processes[i].id == section.process)
+			{
+				responseTime[i] = section.start - processes[i].arrivalTime;
+				totalr += responseTime[i];
+			}
+		}
+	}
+	int c = processes.size();
+	m.averageResponseTime = totalr / c;
+	m.averageTurnAroundTime = totalt / c;
+	m.averageWaitingTime = totalw / c;
+	m.maximumTurnAroundTime = maximum(turnArround);
+	m.maximumWaitingTime = maximum(waitingTime);
+	m.maximumResponseTime=maximum(responseTime);
+	return m;
 }

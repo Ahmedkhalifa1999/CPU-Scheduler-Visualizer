0b02e13a8341668ab27716813b1e15b6c6faa4ae
Nadaaa19 <71625021+NadaHesham19@users.noreply.github.com>
2022-04-19 23:41:20 +0200

    Update Priority.cpp

--- CPU-Scheduler-Visualizer/Priority.cpp
+++ CPU-Scheduler-Visualizer/Priority.cpp
@@ -7,6 +7,7 @@ GanntChart Priority(const std::vector<process>& p, bool preemptive) {
     unsigned int n = p.size();
     vector<unsigned int> burst_remaining(n);
     vector<bool> is_completed(n, false);
+    vector<bool> visited(n, false);
     vector<unsigned int> start_time(n);
     vector<unsigned int> completion_time(n);
     unsigned int current_time = 0;
@@ -53,23 +54,38 @@ GanntChart Priority(const std::vector<process>& p, bool preemptive) {
             if (idx != -1) {
                 if (burst_remaining[idx] == p[idx].burstLength) {
                     start_time[idx] = current_time;
-                    if (prev != -1 && burst_remaining[prev] != 0 && p[idx].priority < p[prev].priority) {
-                        if (temp != -1 && burst_remaining[temp] != 0 && p[prev].burstLength - burst_remaining[prev] > 1 && burst_remaining[idx] == 0) {
+
+                    if (prev != -1 && start_time[prev] == p[idx].arrivalTime - (p[prev].burstLength - burst_remaining[prev]) && burst_remaining[prev] != 0) {/* law kan bad2 men el awaal bs mkhlssh el burst kolo*/
+                        if (visited[prev] != true)
+                            gc.push_back({ p[prev].id,p[idx].arrivalTime - (p[prev].burstLength - burst_remaining[prev]) ,p[idx].arrivalTime });
+
+                        temp = idx;
+                        visited[prev] = true; // lama astkhdmo tany mesh hyb2a bade2 men el awal
+                    }
+                    else if (prev != -1 && visited[prev] == true && completion_time[temp] != current_time) {
+                        gc.push_back({ p[prev].id,completion_time[temp],p[idx].arrivalTime });
+                        temp = idx;
+                    }
+                    /*if (prev != -1 && burst_remaining[prev] != 0 && p[idx].priority < p[prev].priority) {
+                        if (temp!=-1 && burst_remaining[temp]!=0&& p[prev].burstLength-burst_remaining[prev]>1 && burst_remaining[idx] == 0) {
                             gc.push_back({ p[prev].id,completion_time[temp],p[idx].arrivalTime });
 
                             temp = idx;
                         }
                         else if (temp != idx && burst_remaining[idx] >= 1) {
-                            if (p[idx].arrivalTime == current_time && p[idx].priority < p[prev].priority) {
+                            if (p[idx].arrivalTime == current_time && p[idx].priority<p[prev].priority) {
                                 gc.push_back({ p[prev].id,start_time[prev],p[idx].arrivalTime });
                                 temp = idx;
 
                             }
 
                         }
-                    }
-                   
+                    }*/
+                    //lama ykoon lesa awel mara yeshtghal
                 }
+
+
+
 
                 burst_remaining[idx] -= 1;
                 current_time++;
@@ -80,16 +96,20 @@ GanntChart Priority(const std::vector<process>& p, bool preemptive) {
                     completion_time[idx] = current_time;
                     if (start_time[idx] == completion_time[idx] - p[idx].burstLength) {
                         gc.push_back({ p[idx].id,start_time[idx],completion_time[idx] });
-
                         temp = idx;
-                      
+                        visited[idx] = true;
+                        //bada2 men el awal
                     }
                     else {
-                        
-                        gc.push_back({ p[idx].id,completion_time[temp],completion_time[idx] });
+                        //if (temp != idx && temp!=-1) {
+                        gc.push_back({ p[idx].id,completion_time[temp],completion_time[idx] });//lama ykooon bykmel
 
                         temp = idx;
-                        
+                        visited[idx] = true;
+                        // }
+                         /*else if (prev != idx && prev != -1) {
+                             gc.push_back({ p[idx].id,completion_time[prev],completion_time[idx]});
+                         }*/
                     }
 
                     is_completed[idx] = true;
